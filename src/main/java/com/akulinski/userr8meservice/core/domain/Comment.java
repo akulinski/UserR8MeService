6bfcdc3d33971e0aebaf3de6533df73043614f18
Albert Kuliński <albert.kulinski@hotmail.com>
2019-11-19 21:53:41 +0100

    Added No Args constructor to comment

--- src/main/java/com/akulinski/userr8meservice/core/domain/Comment.java
+++ src/main/java/com/akulinski/userr8meservice/core/domain/Comment.java
@@ -3,11 +3,13 @@ package com.akulinski.userr8meservice.core.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Comment implements Serializable {
 
     private String comment;
