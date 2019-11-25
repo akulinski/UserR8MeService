256aa0f0bc1d7232ed0bb4563468fc4349272030
Albert Kuliński <albert.kulinski@hotmail.com>
2019-11-25 19:56:10 +0100

    Added exception handling for validation

--- src/main/java/com/akulinski/userr8meservice/web/rest/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/akulinski/userr8meservice/web/rest/RestResponseEntityExceptionHandler.java
@@ -1,6 +1,9 @@
 package com.akulinski.userr8meservice.web.rest;
 
 import com.akulinski.userr8meservice.core.domain.dto.ExceptionDTO;
+import com.akulinski.userr8meservice.core.exceptions.validation.InvalidEmailException;
+import com.akulinski.userr8meservice.core.exceptions.validation.InvalidPasswordException;
+import com.akulinski.userr8meservice.core.exceptions.validation.InvalidUsernameException;
 import com.mongodb.MongoWriteException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,15 +18,33 @@ import java.util.Date;
 @ControllerAdvice
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
 
-    @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
-    public ResponseEntity handleConflict(RuntimeException ex, WebRequest request) {
-        ExceptionDTO exceptionDTO = new ExceptionDTO("Internal Application Error ", new Date().toInstant());
-        return handleExceptionInternal(ex, exceptionDTO, new HttpHeaders(), HttpStatus.CONFLICT, request);
-    }
+  @ExceptionHandler(value = {IllegalArgumentException.class, IllegalStateException.class})
+  public ResponseEntity handleConflict(RuntimeException ex, WebRequest request) {
+    ExceptionDTO exceptionDTO = new ExceptionDTO("Internal Application Error ", new Date().toInstant());
+    return handleExceptionInternal(ex, exceptionDTO, new HttpHeaders(), HttpStatus.CONFLICT, request);
+  }
 
-    @ExceptionHandler(value = {MongoWriteException.class})
-    public ResponseEntity handleDuplicateKey(MongoWriteException ex, WebRequest request){
-        ExceptionDTO exceptionDTO = new ExceptionDTO(ex.getMessage(), new Date().toInstant());
-        return handleExceptionInternal(ex, exceptionDTO, new HttpHeaders(), HttpStatus.CONFLICT, request);
-    }
+  @ExceptionHandler(value = {MongoWriteException.class})
+  public ResponseEntity handleDuplicateKey(MongoWriteException ex, WebRequest request) {
+    ExceptionDTO exceptionDTO = new ExceptionDTO(ex.getMessage(), new Date().toInstant());
+    return handleExceptionInternal(ex, exceptionDTO, new HttpHeaders(), HttpStatus.CONFLICT, request);
+  }
+
+  @ExceptionHandler(value = {InvalidEmailException.class})
+  public ResponseEntity handleInvalidEmail(InvalidEmailException invalidEmailException, WebRequest webRequest) {
+    ExceptionDTO exceptionDTO = new ExceptionDTO(invalidEmailException.getMessage(), new Date().toInstant());
+    return handleExceptionInternal(invalidEmailException, exceptionDTO, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+  }
+
+  @ExceptionHandler(value = {InvalidPasswordException.class})
+  public ResponseEntity handleInvalidPassword(InvalidPasswordException ex, WebRequest webRequest) {
+    ExceptionDTO exceptionDTO = new ExceptionDTO(ex.getMessage(), new Date().toInstant());
+    return handleExceptionInternal(ex, exceptionDTO, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+  }
+
+  @ExceptionHandler(value = {InvalidUsernameException.class})
+  public ResponseEntity handleInvalidUsername(InvalidUsernameException ex, WebRequest webRequest) {
+    ExceptionDTO exceptionDTO = new ExceptionDTO(ex.getMessage(), new Date().toInstant());
+    return handleExceptionInternal(ex, exceptionDTO, new HttpHeaders(), HttpStatus.BAD_REQUEST, webRequest);
+  }
 }
