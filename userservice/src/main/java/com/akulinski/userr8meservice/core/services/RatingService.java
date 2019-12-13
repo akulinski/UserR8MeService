0a4793bc3a8849f4f958cc6e8bb89d2f2b23a6d5
Albert Kuliński <albert.kulinski@hotmail.com>
2019-12-13 22:39:17 +0100

    Added ansible playbook

--- userservice/src/main/java/com/akulinski/userr8meservice/core/services/RatingService.java
+++ userservice/src/main/java/com/akulinski/userr8meservice/core/services/RatingService.java
@@ -12,7 +12,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.Cacheable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.security.Principal;
